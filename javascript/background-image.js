d0d5c429a58df002d9575bee1f51d0d082bb6775
Yasin <yasinbhojani333@gmail.com>
2022-05-08 19:06:18 +0530

    added principal quote, mission and vision

---
+++ javascript/background-image.js
@@ -0,0 +1,15 @@
+const images = ["auditorium.jpg", "left-garden.jpg", "vpimsr-front5.jpg", "left-garden2.jpg", "memorial1.jpg", "parking2.jpg", "vpimsr-front.jpg"];
+
+const main = document.querySelector('main');
+
+let imagePointer = 0;
+
+function changeBackground () {
+  main.style.backgroundImage = `linear-gradient(rgba(4, 9, 30, 0.7), rgba(4, 9, 30, 0.7)), url(src/college/${images[imagePointer]})`
+
+  imagePointer = (imagePointer + 1) % images.length;
+}
+
+window.onload = function () {
+  setInterval(changeBackground, 5000);
+}
