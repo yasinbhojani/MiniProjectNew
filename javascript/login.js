1fca1cf8674dbb3b31d2c36614364dff80144c44
Yasin <yasinbhojani333@gmail.com>
2022-05-10 21:57:08 +0530

    added function in login page

---
+++ javascript/login.js
@@ -0,0 +1,57 @@
+const login = document.querySelector('.popupbut');
+
+const data = [
+  {
+    name: 'yasin',
+    pass: 'yasin123',
+    git: 'yasinbhojani'
+  },
+  {
+    name: 'sadiq',
+    pass: 'sadiq123',
+    git: 'SadiqhasanRupani72'
+  },
+  {
+    name: 'soham',
+    pass: 'soham123',
+    git: 'SohamGanmote'
+  },
+  {
+    name: 'zakir',
+    pass: 'zakir123',
+    git: 'zakirhusain-3802'
+  }
+]
+
+function changeName(index) {
+  const profile = document.querySelector('#profile');
+  profile.textContent = index.name;
+  profile.style.textTransform = 'capitalize';
+  profile.setAttribute('href',`https://github.com/${index.git}`)
+  profile.setAttribute('target', '_blank');
+
+  profile.addEventListener('click', () => {
+    document.querySelector(".popup").style.display = "none";
+    document.querySelector(".master-container").style.opacity = 1
+  })
+}
+
+login.addEventListener('click', () => {
+  const username = document.querySelector('#username_input').value;
+  const password = document.querySelector('#password_input').value;
+  const msg = document.querySelector('#message');
+
+  for (let index of data) {
+    if (index.name == username && index.pass == password) {
+      msg.style.opacity = '1'
+      msg.textContent = 'Login Successful, You may close this window now'
+      msg.style.color = 'green'
+      changeName(index);
+      break
+    } else {
+      msg.style.opacity = '1'
+      msg.textContent = 'Wrong username or password'
+      msg.style.color = 'red'
+    }
+  }
+})
